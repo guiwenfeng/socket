f1aeac88d0e1af1886961f5d9983a819dafb2036
guiwenfeng <943242261@qq.com>
2017-08-18 11:33:54 +0800

    modified:   getaddrinfo.c

--- getaddrinfo.c
+++ getaddrinfo.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
     
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_socktype = SOCK_STREAM; /* Stream socket */
     hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
     hints.ai_protocol = 0;          /* Any protocol */
     hints.ai_canonname = NULL;
