bac3a679406a641fa680d1b554488f211a128c8e
guiwenfeng <943242261@qq.com>
2017-08-18 16:48:27 +0800

    new file:   clients.c

---
+++ clients.c
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd, s;
+    int i;
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s host port\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+           
+    s = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (s != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+    /* getaddrinfo() returns a list of address structures.
+        Try each address until we successfully connect(2).
+        If socket(2) (or connect(2)) fails, we (close the socket
+        and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
+        for(i = 0; i < 256; i++)
+        {
+            sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+            if (sfd == -1)
+                continue;
+
+            connect(sfd, rp->ai_addr, rp->ai_addrlen);
+        }
+    }
+
+    freeaddrinfo(result);
+    
+    sleep(60);
+}
